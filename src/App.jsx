9fce1b8b2d9b861a45276919339b769af7effb95
Sameerdev512 <sameerkhatridev512@gmail.com>
2025-03-13 12:36:22 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Router from './router/Router'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+
+  return (
+    <>
+      <Router/>
+    </>
+  );
+}
+
+export default App
